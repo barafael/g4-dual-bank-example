9f68e15f660d45fe4e5cc323480f43aae995304c
Bachmann, Rafael <rafael.bachmann@duagon.com>
2020-11-30 11:06:33 +0100

    Run clang-format

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -29,7 +29,8 @@
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-typedef enum {
+typedef enum
+{
     NONE,
     PREPARATION,
     FLASH_ERASING,
@@ -41,11 +42,8 @@ typedef enum {
 
 #define SIZE 20
 uint8_t data[SIZE] = {
-        0x00, 0x11, 0x22, 0x33,
-        0x44, 0x55, 0x66, 0x77,
-        0x88, 0x99, 0xaa, 0xbb,
-        0xcc, 0xdd, 0xee, 0xff,
-        0xaa, 0xaa, 0xaa, 0xaa,
+    0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99,
+    0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff, 0xaa, 0xaa, 0xaa, 0xaa,
 };
 /* USER CODE END PTD */
 
@@ -54,39 +52,11 @@ uint8_t data[SIZE] = {
 
 uint32_t pageError;
 
-bool setData(uint8_t *data, size_t length, uint32_t bank, uint32_t page, uint32_t address) {
-    HAL_FLASH_Unlock();
-    uint64_t *doubleword_data = (uint64_t *)data;
-    uint8_t remainder = length % sizeof(uint64_t);
-    if (remainder != 0) {
-        length -= remainder;
-    }
-    for (size_t index = 0; index < length / 8; index++) {
-        uint64_t doubleword = doubleword_data[index];
-        HAL_FLASH_Program(FLASH_TYPEPROGRAM_DOUBLEWORD, address + (uint8_t)(index * 8), doubleword);
-    }
-    if (remainder != 0) {
-        uint64_t lastDoubleWord = 0;
-        for (size_t index = 0; index < sizeof(uint64_t); index++) {
-            if (index < remainder) {
-                lastDoubleWord |= data[length + index] << (index * 8u);
-            } else {
-                lastDoubleWord |= (uint64_t)0xFF << (index * 8u);
-            }
-        }
-        HAL_FLASH_Program(FLASH_TYPEPROGRAM_DOUBLEWORD, address + length, lastDoubleWord);
-    }
-
-    HAL_FLASH_Lock();
-
-    return true;
-}
-
 // Number of pages for firmware
 #define NUM_PAGES 100
 
 // Number of bytes for firmware
-#define NUM_BYTES       NUM_PAGES * FLASH_PAGE_SIZE
+#define NUM_BYTES NUM_PAGES *FLASH_PAGE_SIZE
 
 // Number of double words (64 bits) for firmware
 #define NUM_DOUBLEWORDS NUM_BYTES / 8
@@ -140,7 +110,7 @@ void toggleBankAndReset() {
     HAL_FLASHEx_OBGetConfig(&OBInit);
 
     OBInit.OptionType = OPTIONBYTE_USER;
-    OBInit.USERType = OB_USER_BFB2;
+    OBInit.USERType   = OB_USER_BFB2;
 
     if (((OBInit.USERConfig) & (OB_BFB2_ENABLE)) == OB_BFB2_ENABLE) {
         OBInit.USERConfig = OB_BFB2_DISABLE;
@@ -172,34 +142,33 @@ uint8_t getActiveBank() {
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
-  /* USER CODE BEGIN 1 */
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
+    /* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+    /* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+    /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
 
-  /* USER CODE BEGIN Init */
+    /* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+    /* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+    /* Configure the system clock */
+    SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+    /* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+    /* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  /* USER CODE BEGIN 2 */
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    /* USER CODE BEGIN 2 */
     uint8_t bank = getActiveBank();
 
     uint32_t last = HAL_GetTick();
@@ -210,10 +179,10 @@ int main(void)
     } else {
         delay = 500;
     }
-  /* USER CODE END 2 */
+    /* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
     while (1) {
         uint32_t now = HAL_GetTick();
         if (now - last > delay) {
@@ -226,179 +195,156 @@ int main(void)
             }
         }
         switch (updateState) {
-        case NONE:
-            break;
-        case PREPARATION: {
-            FLASH_EraseInitTypeDef erase = { 0 };
-            //erase.TypeErase = FLASH_TYPEERASE_PAGES;
-            erase.TypeErase = FLASH_TYPEERASE_MASSERASE;
-            erase.Banks = bank == 1 ? FLASH_BANK_2 : FLASH_BANK_1;
-            //erase.NbPages = NUM_PAGES;
-            //erase.Page = 0;
+            case NONE:
+                break;
+            case PREPARATION: {
+                FLASH_EraseInitTypeDef erase = {0};
+                //erase.TypeErase = FLASH_TYPEERASE_PAGES;
+                erase.TypeErase = FLASH_TYPEERASE_MASSERASE;
+                erase.Banks     = bank == 1 ? FLASH_BANK_2 : FLASH_BANK_1;
+                //erase.NbPages = NUM_PAGES;
+                //erase.Page = 0;
 
-            HAL_FLASH_Unlock();
-            HAL_StatusTypeDef status = HAL_FLASHEx_Erase_IT(&erase);
-            if (status != HAL_OK) {
-                // TODO error case
+                HAL_FLASH_Unlock();
+                HAL_StatusTypeDef status = HAL_FLASHEx_Erase_IT(&erase);
+                if (status != HAL_OK) {
+                    // TODO error case
+                }
+
+                updateState = FLASH_ERASING;
+            } break;
+
+            case FLASH_ERASING:
+            case FLASH_WRITE_IN_PROGRESS:
+                delay = 200;
+                break;
+
+            case FLASH_ERASE_DONE:
+            case FLASH_WRITE_DONE: {
+                delay               = 50;
+                static size_t index = 0;
+
+                uint32_t dest = 0x08040000;
+                uint8_t *src  = (uint8_t *)0x08000000;
+
+                if (index < NUM_DOUBLEWORDS) {
+                    uint64_t doubleword = *(uint64_t *)(src + (index * 8));
+
+                    updateState = FLASH_WRITE_IN_PROGRESS;
+                    HAL_FLASH_Program_IT(FLASH_TYPEPROGRAM_DOUBLEWORD, dest + index * 8, doubleword);
+                    index++;
+                } else {
+                    updateState = UPDATE_DONE;
+                }
+            } break;
+
+            case UPDATE_DONE: {
+                HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+                if (getActiveBank() == 1) {
+                    uint64_t *doubleword_data = (uint64_t *)data;
+                    for (size_t index = 0; index < SIZE / 8; index++) {
+                        uint64_t doubleword = doubleword_data[index];
+                        HAL_FLASH_Program(FLASH_TYPEPROGRAM_DOUBLEWORD, 0x0807F000 + (uint8_t)(index * 8), doubleword);
+                    }
+                } else {
+                    uint64_t *doubleword_data = (uint64_t *)data;
+                    for (size_t index = 0; index < SIZE / 8; index++) {
+                        uint64_t doubleword = doubleword_data[index];
+                        HAL_FLASH_Program(FLASH_TYPEPROGRAM_DOUBLEWORD, 0x0807F000 + (uint8_t)(index * 8), doubleword);
+                    }
+                }
+                toggleBankAndReset();
+                break;
             }
-
-            updateState = FLASH_ERASING;
         }
-            break;
+        /* USER CODE END WHILE */
 
-        case FLASH_ERASING:
-        case FLASH_WRITE_IN_PROGRESS:
-            delay = 200;
-            break;
-
-        case FLASH_ERASE_DONE:
-        case FLASH_WRITE_DONE: {
-            delay = 50;
-            static size_t index = 0;
-
-            uint32_t dest = 0x08040000;
-            uint8_t *src = (uint8_t*) 0x08000000;
-
-            if (index < NUM_DOUBLEWORDS) {
-                uint64_t doubleword = *(uint64_t*) (src + (index * 8));
-
-                updateState = FLASH_WRITE_IN_PROGRESS;
-                HAL_FLASH_Program_IT(
-                        FLASH_TYPEPROGRAM_DOUBLEWORD,
-                        dest + index * 8,
-                        doubleword);
-                index++;
-            } else {
-                updateState = UPDATE_DONE;
-            }
-        }
-            break;
-
-        case UPDATE_DONE: {
-				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
-				HAL_Delay(1000);
-				HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-				HAL_Delay(1000);
-				HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-				HAL_Delay(1000);
-				HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-				HAL_Delay(1000);
-				if (getActiveBank() == 1) {
-					//setData(data, SIZE, FLASH_BANK_2, 254, (uint32_t)0x0807F000);
-					uint64_t *doubleword_data = (uint64_t *)data;
-					for (size_t index = 0; index < SIZE / 8; index++) {
-						uint64_t doubleword = doubleword_data[index];
-						HAL_FLASH_Program(FLASH_TYPEPROGRAM_DOUBLEWORD,
-								0x0807F000 + (uint8_t)(index * 8),
-								doubleword);
-					}
-				} else {
-					uint64_t *doubleword_data = (uint64_t *)data;
-					for (size_t index = 0; index < SIZE / 8; index++) {
-						uint64_t doubleword = doubleword_data[index];
-						HAL_FLASH_Program(FLASH_TYPEPROGRAM_DOUBLEWORD,
-								0x0807F000 + (uint8_t)(index * 8),
-								doubleword);
-					}
-				}
-				toggleBankAndReset();
-				break;
-			}
-        }
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
+        /* USER CODE BEGIN 3 */
     }
-  /* USER CODE END 3 */
+    /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
-  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV2;
-  RCC_OscInitStruct.PLL.PLLN = 25;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
-  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    /** Configure the main internal regulator output voltage
+     */
+    HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
+    /** Initializes the RCC Oscillators according to the specified parameters
+     * in the RCC_OscInitTypeDef structure.
+     */
+    RCC_OscInitStruct.OscillatorType      = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState            = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource       = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLM            = RCC_PLLM_DIV2;
+    RCC_OscInitStruct.PLL.PLLN            = 25;
+    RCC_OscInitStruct.PLL.PLLP            = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ            = RCC_PLLQ_DIV2;
+    RCC_OscInitStruct.PLL.PLLR            = RCC_PLLR_DIV2;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
+    /** Initializes the CPU, AHB and APB buses clocks
+     */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+        Error_Handler();
+    }
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_GPIO_Init(void) {
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOF_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
+    /* GPIO Ports Clock Enable */
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOF_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+    /*Configure GPIO pin Output Level */
+    HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : B1_Pin */
-  GPIO_InitStruct.Pin = B1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+    /*Configure GPIO pin : B1_Pin */
+    GPIO_InitStruct.Pin  = B1_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : LPUART1_TX_Pin LPUART1_RX_Pin */
-  GPIO_InitStruct.Pin = LPUART1_TX_Pin|LPUART1_RX_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  GPIO_InitStruct.Alternate = GPIO_AF12_LPUART1;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    /*Configure GPIO pins : LPUART1_TX_Pin LPUART1_RX_Pin */
+    GPIO_InitStruct.Pin       = LPUART1_TX_Pin | LPUART1_RX_Pin;
+    GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull      = GPIO_NOPULL;
+    GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF12_LPUART1;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : LD2_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+    /*Configure GPIO pin : LD2_Pin */
+    GPIO_InitStruct.Pin   = LD2_Pin;
+    GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull  = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
 
-  /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
-
+    /* EXTI interrupt init*/
+    HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 }
 
 /* USER CODE BEGIN 4 */
@@ -406,31 +352,29 @@ static void MX_GPIO_Init(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+    /* USER CODE BEGIN Error_Handler_Debug */
     /* User can add his own implementation to report the HAL error return state */
 
-  /* USER CODE END Error_Handler_Debug */
+    /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line) {
+    /* USER CODE BEGIN 6 */
+    /* User can add his own implementation to report the file name and line number,
+       tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+    /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
 
